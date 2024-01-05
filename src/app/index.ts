5846a3342f4c183431eee939137f9b6a8df1ad69
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-01-05 19:07:24 +0530

    added auth flow

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,8 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { User } from "./user";
 import cors from "cors";
+import { GraphqlContext } from "../interfaces";
+import JWTService from "../services/jwt";
 
 export async function initServer() {
   const app = express();
@@ -11,7 +13,7 @@ export async function initServer() {
   app.use(bodyParser.json());
   app.use(cors());
 
-  const graphqlServer = new ApolloServer({
+  const graphqlServer = new ApolloServer<GraphqlContext>({
     typeDefs: `
         ${User.types}
         type Query {
@@ -27,7 +29,20 @@ export async function initServer() {
 
   await graphqlServer.start();
 
-  app.use("/graphql", expressMiddleware(graphqlServer));
+  app.use(
+    "/graphql",
+    expressMiddleware(graphqlServer, {
+      context: async ({ req, res }) => {
+        return {
+          user: req.headers.authorization
+            ? JWTService.decodeToken(
+                req.headers.authorization.split("Bearer ")[1]
+              )
+            : undefined,
+        };
+      },
+    })
+  );
 
   return app;
 }
--- src/services/jwt.ts
+++ src/services/jwt.ts
@@ -4,17 +4,26 @@ const JWT_SECRET: string = process.env.JWT_SECRET || "";
 
 import { prismaClient } from "../clients/db";
 import { User } from "@prisma/client";
+import { JWTUser } from "../interfaces";
 
 class JWTService {
   public static generateTokenForUser(user: User) {
     // const user = await prismaClient.user.findUnique({ where: { id: userId } });
-    const payload = {
+    const payload: JWTUser = {
       id: user?.id,
       email: user?.email,
     };
 
     const token = JWT.sign(payload, JWT_SECRET);
     return token;
+  }
+
+  public static decodeToken(token: string) {
+    try {
+      return JWT.verify(token, JWT_SECRET) as JWTUser;
+    } catch (error) {
+      return null;
+    }
   }
 }
 
