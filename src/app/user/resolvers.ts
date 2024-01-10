dd8110504921e05b40541d53da814ad84e6132c2
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-01-10 13:44:20 +0530

    added followers/following

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -41,7 +41,57 @@ const extraResolvers = {
           },
         },
       }),
+    followers: async (parent: User) => {
+      const result = await prismaClient.follows.findMany({
+        where: {
+          following: {
+            id: parent.id,
+          },
+        },
+        include: {
+          follower: true,
+        },
+      });
+      return result.map((el) => el.follower);
+    },
+    following: async (parent: User) => {
+      const result = await prismaClient.follows.findMany({
+        where: {
+          follower: {
+            id: parent.id,
+          },
+        },
+        include: {
+          following: true,
+        },
+      });
+
+      return result.map((el) => el.following);
+    },
   },
 };
 
-export const resolvers = { queries, extraResolvers };
+const mutations = {
+  followUser: async (
+    parent: any,
+    { to }: { to: string },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user || !ctx.user.id)
+      throw new Error("You are not authenticated.");
+    await UserService.followUser(ctx.user.id, to);
+    return true;
+  },
+  unfollowUser: async (
+    parent: any,
+    { to }: { to: string },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user || !ctx.user.id)
+      throw new Error("You are not authenticated.");
+    await UserService.unfollowUser(ctx.user.id, to);
+    return true;
+  },
+};
+
+export const resolvers = { queries, extraResolvers, mutations };
--- src/services/user.ts
+++ src/services/user.ts
@@ -70,6 +70,26 @@ class UserService {
 
     return user;
   };
+
+  public static followUser(from: string, to: string) {
+    return prismaClient.follows.create({
+      data: {
+        follower: { connect: { id: from } },
+        following: { connect: { id: to } },
+      },
+    });
+  }
+
+  public static unfollowUser(from: string, to: string) {
+    return prismaClient.follows.delete({
+      where: {
+        followerId_followingId: {
+          followerId: from,
+          followingId: to,
+        },
+      },
+    });
+  }
 }
 
 export default UserService;
